82e69c33a6002a99a05219030b7c7c75c4b3046f
Christian Agha <christianagha@gmail.com>
2017-04-12 18:34:09 +0000

    changes to the copy func

--- list_to_2d_array.c
+++ list_to_2d_array.c
@@ -4,7 +4,8 @@ char ** conv_list_to_array(env_var_list_t * env_head, int n)
 {
 	char **array;
 	env_var_list_t *tmp;
-	int i, len, nodes;
+	int i, len, nodes, j;
+	char * val;
 
 	i = len = nodes = 0;
 	tmp = env_head;
@@ -15,21 +16,24 @@ char ** conv_list_to_array(env_var_list_t * env_head, int n)
 		nodes++;
 		printf("i is %d\n", i--);
 	}
-	printf("right aftet the secong");
 	tmp = env_head;
 	array = malloc(sizeof(char *) * (nodes + 1));
+	i = 0;
 	while (tmp != NULL)
 	{
-		printf("tmp is NOTTTTTT NULL!!!!!!");
 		len = _strlen(tmp->key) + _strlen(tmp->value) + 2;
 		array[i] = malloc(sizeof(char) * len);
 		_memset(array[i], '\0', len);
-		_memcopy(array[i], tmp->key, sizeof(tmp->key));
-/*		_memcopy(array[i][(char *)sizeof(tmp->key)], '=', 1);
-		_memcopy(array[i][sizeof(tmp->key) + 1], tmp->value, sizeof(tmp->value));*/
+		_memcopy(array[i], tmp->key, _strlen(tmp->key));
+		array[i][_strlen(tmp->key)] = '=';
+		val = tmp->value;
+		for (j = 0; j < _strlen(tmp->value); j++)
+		{
+			array[i][_strlen(tmp->key) + 1 + j] = val[j];
+		}
+		printf("%s\n", array[i]);
 		tmp = tmp->next;
 		i++;
-		printf("%s\n", array[i]);
 	}
 	array[i] = NULL;
 	return(array);
