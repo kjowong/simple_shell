ac29b5be8fe9acf07cc075b5c672d6b5ea42fc47
Christian Agha <christianagha@gmail.com>
2017-04-15 07:41:11 +0000

    fixec memory read error

--- shell.c
+++ shell.c
@@ -8,19 +8,20 @@ int main(void)
 	env_var_list_t *env_head;
 	char **enviroment_list, **path_array, **input_array;
 	int words = 0;
-	struct stat sb;
-	int read, pipe = 0;
+	/*struct stat sb;*/
+	int read;/*, pipe = 0;*/
 
 	len = i = num_of_env_nodes = 0;
 	enviroment_list = path_array = input_array = NULL;
 	env_head = NULL;
 	input_head = NULL;
 	buffer = NULL;
+
 	num_of_env_nodes = create_env_list(&env_head);
 	enviroment_list = conv_list_to_array(env_head, num_of_env_nodes);
 	path_array = path_parserator(env_head);
 
-	if (fstat(STDIN_FILENO, &sb) == -1)
+	/*if (fstat(STDIN_FILENO, &sb) == -1)
 	{
 		perror("Fail Status");
 		exit(EXIT_FAILURE);
@@ -29,22 +30,27 @@ int main(void)
 	{
 		pipe = 1;
 	}
-	if (pipe == 0)
-		_write("B-shell$$$ ");
+	if (pipe == 0)*/
+	_write("B-shell$$$ ");
 	while ((read = getline(&buffer, &len, stdin)) != -1)
 	{
+		/*buffer = input_parserator(buffer);*/
+		if(_strcmp(buffer, "exit") == 0)
+			break;
 		if (_strcmp(buffer, "\n") == 0 || _strcmp(buffer, "\t") == 0)
 		{
 			_write("B-shell$$$ ");
 			continue;
 		}
+
 		words = input_word_counter(buffer);
-		if(_strcmp(buffer, "exit") == 0 || read < 0)
-			break;
-		input_array = input_to_array(buffer, words);
-		cmd_executor(path_array, input_array);
+		if (words > 0)
+		{
+			input_array = input_to_array(buffer, words);
+			cmd_executor(path_array, input_array);
+		}
 		free(input_array);
-		if (pipe == 0)
+		/*if (pipe == 0)*/
 			_write("B-shell$$$ ");
 	}
 	free_mem(buffer, input_head, env_head, enviroment_list, path_array);
