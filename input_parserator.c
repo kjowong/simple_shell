aa3b9bca11775324e12ca9208f0c1db3e2a4433d
Kimberly Wong <kjowong@gmail.com>
2017-04-18 18:09:05 +0000

    Style fix, function ready

--- input_parserator.c
+++ input_parserator.c
@@ -1,18 +1,22 @@
 #include "shell.h"
-
+/**
+  * input_parserator - parese the input
+  * @input_buffer: buffer that holds the input
+  * Return: returns the input, parsed
+  */
 char *input_parserator(char *input_buffer)
 {
 	char *parsed_input;
 	int i;
+
 	parsed_input = _strdup(input_buffer);
-	for(i = 0; parsed_input[i] != '\0'; i++)
+	for (i = 0; parsed_input[i] != '\0'; i++)
 	{
-		while(input_buffer[i] == ' ')
+		while (input_buffer[i] == ' ')
 			i++;
 		parsed_input = &input_buffer[i];
 	}
-	printf("parsed_input: %s\n", parsed_input);
 	free(input_buffer);
-	return(parsed_input);
+	return (parsed_input);
 
 }
