449de4a4c287c297eac7aa292da1d7f69753a696
Christian Agha <christianagha@gmail.com>
2017-04-19 23:05:56 +0000

    chaged style fix

--- input_to_array.c
+++ input_to_array.c
@@ -1,19 +1,26 @@
 #include "shell.h"
+/**
+ * input_to_array - converts inout to a double pointer array
+ * @buffer: string input
+ * @tokens: number of tokens in string
+ * Return - pointer to new input array
+ **/
 
-char **input_to_array(char *buffer, int words)
+char **input_to_array(char *buffer, int tokens)
 {
 	int i;
 	char **input_array;
-	input_array = _grand_malloc(sizeof(char *) * (words + 1));
-	for(i = 0; i < words; i++)
+
+	input_array = _grand_malloc(sizeof(char *) * (tokens + 1));
+	for (i = 0; i < tokens; i++)
 	{
 		if (i == 0)
-			input_array[i] = strtok(buffer, " \n\t\r\"");
+			input_array[i] = strtok(buffer, "\n\t\r\" ");
 		else
-			input_array[i] = strtok(NULL, " \n\t\r\"");
+			input_array[i] = strtok(NULL, "\n\t\r\" ");
 	}
 	if (input_array[i - 1] != NULL)
 		input_array[i] = NULL;
 
-	return(input_array);
+	return (input_array);
 }
